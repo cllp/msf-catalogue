c8d3810b9da3563eae65b9ba95aa2dd31671860a
congvu <congvu_88@hotmail.com>
2017-03-22 13:54:41 +0100

    tested connection

---
+++ db/Domain/WebApplication/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+using WebApplication.Repositories;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/product")]
+    public class ProductController :  Controller
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        [Route("All")]
+        [HttpGet]
+        // GET api/product
+        public IEnumerable<Product> Get()
+        {
+            return _productRepository.GetAll();
+        }
+
+        // GET api/product/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }   
+}
---
+++ db/Domain/WebApplication/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Repositories
+{
+    public class ProductRepository
+    {
+        public string connectionString;
+
+        public ProductRepository()
+        {
+            //connectionString = "Data Source=MMA-4CZ13909R0; SQLEXPRESS01;Initial Catalog=MIC_Data;Integrated Security=True";
+            //connectionString = @"Server = (localdb)\\mssqllocaldb; SQLEXPRESS; Database = MIC_Data; Trusted_Connection = True; MultipleActiveResultSets = true";
+            connectionString = "Data Source=MMA-4CZ13909R0\\SQLEXPRESS01;Initial Catalog=MIC_Data;Integrated Security=True";
+
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+              return dbConnection.Query<Product>("SELECT PRODUCTTYPEID,PRODUCTNAME FROM PRODUCT");
+            }
+        }
+    }
+}
