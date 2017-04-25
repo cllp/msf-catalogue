980e332c77f69c73620a11b460f057f10295032d
mohamedalgalby <mohamed.algalby1@gmail.com>
2017-04-25 10:49:59 -0700

    Filter done

---
+++ MSF.LogisticsPlatform.Domain/Infrastructure/DBConnectionFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace MSF.LogisticsPlatform.Domain.Infrastucture
+{
+    public class DBConnectionFactory:IDBConnectionFactory,IDisposable
+    {
+        //To connent Azure SQL server just change the conn. String
+
+        private readonly string connectionString = @"Data Source=JAYWAY-PC;Initial Catalog=MIC_Data;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
